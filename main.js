0696243333a3aa2cd2261739d0bb2ef26a854799
Kryštof <krystof.kratky2003@gmail.com>
2021-06-03 10:13:02 +0200

    get references on webpages

--- main.js
+++ main.js
@@ -1,19 +1,29 @@
 const fs = require("fs");
 const https = require("https");
 
-let rawdata = fs.readFileSync("schoolsRaw.json");
-let schools = JSON.parse(rawdata);
+let schools = JSON.parse(fs.readFileSync("schoolsRaw.json"));
 
-const write = (d) => {
-  console.log(d.toString());
-};
+var websiteContent;
 
-https
-  .get("https://www.delta-skola.cz", (res) => {
-    res.on("data", (d) => {
-      write(d);
+schools.map((x) => {
+  const getHrefs = (d) => {
+    const regex = /href=["']([^"']*)["']/g;
+    const content = d.toString();
+    const found = content.match(regex);
+    console.log(found);
+  };
+
+  https
+    .get("https://" + x.www.split(",")[0], (res) => {
+      console.log("statusCode:", res.statusCode);
+      console.log("headers:", res.headers);
+
+      res.on("data", (d) => {
+        getHrefs(d);
+      });
+    })
+    .on("error", (e) => {
+      console.error(e);
     });
-  })
-  .on("error", (e) => {
-    console.error(e);
-  });
+});
+console.log("done");
