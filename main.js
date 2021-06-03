0529f1ca5a4dee12cb47c2e9fbbd6e0a10c4b108
Kryštof <krystof.kratky2003@gmail.com>
2021-06-03 09:13:47 +0200

    get site content with https request

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
 const fs = require("fs");
+const https = require("https");
 
-let rawdata = fs.readFileSync("schools.json");
+let rawdata = fs.readFileSync("schoolsRaw.json");
 let schools = JSON.parse(rawdata);
+
+const write = (d) => {
+  console.log(d.toString());
+};
+
+https
+  .get("https://www.delta-skola.cz", (res) => {
+    res.on("data", (d) => {
+      write(d);
+    });
+  })
+  .on("error", (e) => {
+    console.error(e);
+  });
